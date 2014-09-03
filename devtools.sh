09a8a5b18c68b8b4814a561a271abad382e03d86
Carlos Ortega Huetos <carlos@rechannel.co.uk>
2014-09-03 13:36:37 +0100

    Add git tools

---
+++ devtools.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+function askQuestion ()
+{
+    yes="y"
+    no="n"
+    if [ "$2" == "Y" ]; then
+        yes="Y"
+    else
+        no="N"
+    fi
+
+    echo -en "\033[33m$1? [$yes/$no] \033[0m"
+    read decision
+
+    if [ "$2" = "Y" ]; then
+        if [ "$decision" = "" ] || [ "$decision" = "Y" ] || [ "$decision" = "y" ]; then
+            echo true
+        else
+            echo false
+        fi
+    else
+        if [ "$decision" = "" ] || [ "$decision" = "N" ] || [ "$decision" = "n" ]; then
+            echo true
+        else
+            echo false
+        fi
+    fi
+}
+
+if [ -z "$BASE_PATH" ]; then
+    BASE_PATH="~/.bash-dev-tools/"
+fi
+
+GIT_TOOLS="${BASE_PATH}gittools.sh"
+
+if [ "$ENABLE_GIT" = true ]; then
+    source ${GIT_TOOLS}
+fi
---
+++ gittools.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+function getGitBranch ()
+{
+    GIT_SYM=`git symbolic-ref HEAD`
+    echo "${GIT_SYM##refs/heads/}"
+}
+
+function gitCreateCommit ()
+{
+    git add --all
+    git commit -m "$1" --quiet
+}
+
+function gitPushChanges ()
+{
+    git push origin $(getGitBranch) --quiet
+}
+
+function cleanGitEnvironment ()
+{
+    if [ "`askQuestion 'Are you sure you want to clean your environment' 'Y'`" = true ]; then
+        git clean -dfq
+        git checkout -- .
+
+        echo -e "\033[32mEnvironment clean\033[0m"
+    else
+        echo -e "\033[31mAborted...\033[0m"
+    fi
+}
+
+if [ "$ENABLE_ALIAS" = true ]; then
+    alias gclean="\$(cleanGitEnvironment)"
+    alias gclone="git clone"
+    alias gstatus="git status"
+    alias gpush="git push origin \$(getGitBranch)"
+    alias gpull="git pull origin \$(getGitBranch)"
+    alias gadd="git add"
+    alias gcommit="git commit"
+    alias gco="git checkout"
+    alias gfetch="git fetch"
+    alias gtree="git log --graph --pretty=oneline --abbrev-commit"
+    alias gdiff="git diff"
+    alias gmerge="git merge"
+    alias gbranch="git branch"
+    alias gstash="git stash"
+    alias grebase="git rebase"
+    alias greset="git reset"
+    alias grm="git rm"
+fi
