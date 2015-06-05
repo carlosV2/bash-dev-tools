0b3ffb42053ecca275ec29f93ee6a36ec75a6637
Carlos Ortega Huetos <carlos@rechannel.co.uk>
2015-06-05 18:18:33 +0100

    Add a vagrant command for composer

--- vagrant.sh
+++ vagrant.sh
@@ -9,4 +9,5 @@ if [ -n "$ENABLE_ALIAS" ] && [ "$ENABLE_ALIAS" = true ]; then
     alias vmysql="executeCommandInVagrant mysql"
     alias vmongo="executeCommandInVagrant mongo"
     alias vcmd="executeCommandInVagrant"
+    alias vcomposer="executeCommandInVagrant composer"
 fi
