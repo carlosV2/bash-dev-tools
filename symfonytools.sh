eb6a191d57937f06caec7d1f16cb4dcd20715931
Carlos Ortega Huetos <carlos@rechannel.co.uk>
2015-08-13 11:45:17 +0100

    Enable composer quick start

--- symfonytools.sh
+++ symfonytools.sh
@@ -20,4 +20,21 @@ if [ -n "$ENABLE_ALIAS" ] && [ "$ENABLE_ALIAS" = true ]; then
     alias clogs="rm -rf app/logs/*"
     alias call="ccache && clogs"
     alias ac="app/console"
+
+    function composer ()
+    {
+        if [ ! -f "composer.phar" ]; then
+            curl -sS https://getcomposer.org/installer | php > /dev/null 2>&1
+            if [ ! -f "composer.phar" ]; then
+                echo -e "\033[31mUnable to download a copy of composer.\033[0m"
+                return 1;
+            fi
+        fi
+
+        php composer.phar self-update > /dev/null 2>&1
+
+        php composer.phar "$@"
+
+        return $?
+    }
 fi
